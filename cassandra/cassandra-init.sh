e6b93e73d859169204794a5707a42c47a02a97fa
Maliha Sameen <maliha.sameen@bentley.com>
2023-05-25 21:49:42 +0500

    Fixed \r issue

--- cassandra/cassandra-init.sh
+++ cassandra/cassandra-init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-until printf "" 2>>/dev/null >>/dev/tcp/cassandra/9042; do 
+until printf "" 2>>/dev/null >>/dev/tcp/cassandra/9042; do
     sleep 5;
     echo "Waiting for cassandra...";
 done
--- superset/superset-init.sh
+++ superset/superset-init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# create Admin user, you can read these values from env or anywhere else possible.
+# create Admin user, you can read these values from env or anywhere else possible
+
 echo "Creating admin user ${ADMIN_USERNAME} email ${ADMIN_EMAIL}"
 superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD"
 
-# Upgrading Supersetset metastore.
+# Upgrading Superset metastore
 echo "Upgrading DB"
 superset db upgrade
 
 echo "Setup roles"
-superset superset init  #setup roles and permissions
+superset superset init # setup roles and permissions
 
 echo "Starting server"
 /bin/sh -c /usr/bin/run-server.sh
